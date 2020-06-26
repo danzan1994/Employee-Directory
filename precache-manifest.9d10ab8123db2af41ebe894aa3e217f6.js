6cd27f836a307ed094cd8a312c34e4ec7e3d67a6
Danzan Ubush <danzan1994@gmail.com>
2020-06-26 13:25:21 -0400

    Updates

---
+++ precache-manifest.9d10ab8123db2af41ebe894aa3e217f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcbb76cf706ccf314d92e1db892620d",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "5370754b8b2c6c58d4b8",
+    "url": "/Employee-Directory/static/css/main.e4825e3f.chunk.css"
+  },
+  {
+    "revision": "e33d5eec6b2fc73d3bd0",
+    "url": "/Employee-Directory/static/js/2.5eb6328c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.5eb6328c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5370754b8b2c6c58d4b8",
+    "url": "/Employee-Directory/static/js/main.8459db3f.chunk.js"
+  },
+  {
+    "revision": "88776d5770f0e6852f66",
+    "url": "/Employee-Directory/static/js/runtime-main.28620774.js"
+  }
+]);
