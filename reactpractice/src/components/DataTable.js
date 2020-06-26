c0fdb8183caadb54dccffb40b0b0fe02a911687c
Danzan Ubush <danzan1994@gmail.com>
2020-06-25 23:43:57 -0400

    commit once more

---
+++ reactpractice/src/components/DataTable.js
@@ -0,0 +1,48 @@
+import React from "react";
+import DataBody from "./DataBody";
+import "../styles/DataTable.css";
+
+function DataTable({ headings, users, handleSort }) {
+  return (
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="table table-striped table-hover table-condensed"
+      >
+        <thead>
+          <tr>
+
+            {DataTable.headings.map(({name,width}) => {
+         {/* ADD A LINE OF CODE TO LOOP THROUGH HEADINGS WITH .map METHOD
+                DESTRUCTURE INPUT ARGUMENT TO EXTRACT NAME and WIDTH */}
+            {/* THE FOLLOWING RETURN STATEMENT SHOULD BE INSIDE
+                OF .MAP CALLBACK BODY */}
+              return (
+                <th
+                  className="col"
+                  // ADD KEY AND ASSIGN IT WITH NAME PASSED IN
+                  key={name}
+                  // ADD STYLE FOR WIDTH WITH WIDTH PASSED IN
+                  style={{width}}
+                  // ADD ONCLICK EVENT TO CALL CALLBACK PASSED IN
+                  onClick={() => {
+                    DataTable.handleSort(name);
+                  }}
+                >
+                  {/* ADD HEADING NAME PASSED IN HERE */}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })} 
+          </tr>
+        </thead>
+       
+       {/* ADD CODE TO CALL COMPONENT DATABODY PASSING USERS AS INPUT ARGUMENT  */}
+        <DataBody />
+
+      </table>
+    </div>
+  );
+}
+
+export default DataTable;
---
+++ reactpractice/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchBox from "./SearchBox.js";
+import "../styles/Nav.css";
+
+function Nav({ handleSearchChange }) {
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse row" id="navbarNav">
+        <Searchbox
+          handleSearchChange={handleSearchChange}
+        />
+      </div>
+    </nav>
+  );
+}
+export default Nav;
